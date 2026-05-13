b84e4973938db00a1480a2048abf489e432a892a
leomora-ux <leonardo.mora.trento@gmail.com>
2026-05-13 14:10:38 +0200

    firebase js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Per il Login utente
+import { getFirestore } from "firebase/firestore"; // Per il Database delle ricette
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALIrTFwhkUrnEvuC7BUSTznwEfywugIZU",
+  authDomain: "cooked-5ccd9.firebaseapp.com",
+  projectId: "cooked-5ccd9",
+  storageBucket: "cooked-5ccd9.firebasestorage.app",
+  messagingSenderId: "38214822",
+  appId: "1:38214822:web:5cbbd96ac92c7b237ba867",
+  measurementId: "G-VX2ZN0J16L"
+};
+
+// Inizializziamo Firebase
+const app = initializeApp(firebaseConfig);
+
+// Esportiamo i servizi che useremo nelle schermate (views)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
