9d8fa4b07078f9f0d98156bf4eb5b1ece4d7139a
Yuhadzi <hadziyannick90@gmail.com>
2026-07-03 10:55:43 +0200

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 import App from './App.vue'
 import router from './router/index'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App).use(router).mount('#app')
